5ceb215fb03914edf3f8de32af338f007eb0312c
tanghaotian <89062432+tanghaoxu@users.noreply.github.com>
2022-08-16 23:11:27 -0400

    Update ips.js

--- ips.js
+++ ips.js
@@ -204,7 +204,7 @@ function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
         `节点ISP：${info.isp}\n` +
         `节点位置：${getFlagEmoji(info.countryCode)} | ${info.country} - ${info.city}`,
       icon: getSSID() ? 'wifi' : 'simcard',
-      'icon-color': getSSID() ? '#FFC0CB' : '#FF00FF',
+      'icon-color': getSSID() ? '#00FF00' : '#FF00FF',
     });
   }).catch(error => {
     // 网络切换
