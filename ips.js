6219afa3dc9c7f46a048b41fb1264c2594979394
TOM <89062432+TOM69JK@users.noreply.github.com>
2023-02-10 11:20:58 +0800

    Update ips.js

--- ips.js
+++ ips.js
@@ -177,7 +177,6 @@ function getIP() {
   } else {
     if (v4?.primaryAddress) info.push(`设备IP：${v4?.primaryAddress}`);
     if (v6?.primaryAddress) info.push(`IPv6地址：已分配`);
-    if (v4?.primaryRouter && getSSID()) info.push(`路由器IP：${v4?.primaryRouter}`);
     if (v6?.primaryRouter && getSSID()) info.push(`IPv6地址：已分配`);
   }
   info = info.join("\n");
