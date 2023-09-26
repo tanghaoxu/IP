15aece41bf3e852eb400d0f4e2b7df7d2839df96
TOM <89062432+tom-proxy@users.noreply.github.com>
2023-09-26 22:12:11 +0800

    Update ips.js

--- ips.js
+++ ips.js
@@ -204,7 +204,7 @@ function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
         getIP() +
         `节点IP➠${info.query}\n` +
         `节点ISP➠${info.isp}\n` +
-        `坐标➠${getFlagEmoji(info.countryCode)}${info.country}🏰${info.city}🏠${info.zip}`,
+        `坐标➠${getFlagEmoji(info.countryCode)}${info.country}🏰${info.city}📮${info.zip}`,
       icon: getSSID() ? 'wifi' : 'simcard.2.fill',
       'icon-color': getSSID() ? '#00FFFF' : '#E6B422',
     });
